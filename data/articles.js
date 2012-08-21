80f337f1825c23a31dc0764ae741b97bab679854
Stephen Fulljames <stephen@fulljames.net>
2012-08-21 22:08:18 +0100

    Parameters for custom css and js in articles. Initial content file for first article. Inclusion of marked for markdown parsing in articles.

--- data/articles.js
+++ data/articles.js
@@ -2,5 +2,9 @@ exports.items = [{
     'href': 'on-cors',
     'label': 'On Cross-Origin-Resource-Sharing',
     'meta': '21 Aug 2012',
-    'abstract': 'Nulla facilisi. Duis aliquet egestas purus in blandit. Curabitur vulputate, ligula lacinia scelerisque tempor, lacus lacus ornare ante, ac egestas est.'
+    'abstract': 'Nulla facilisi. Duis aliquet egestas purus in blandit. Curabitur vulputate, ligula lacinia scelerisque tempor, lacus lacus ornare ante, ac egestas est.',
+    // custom css for this article
+    'css': '/content/on-cors/article.css',
+    // custom js for this article
+    'js': null
 }];
