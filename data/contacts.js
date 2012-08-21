bcd94036ee136ef17860cd5059c6d936d32ab913
Stephen Fulljames <stephen@fulljames.net>
2012-08-21 14:22:23 +0100

    Extracted content data used in homepage to independent files to be able to include elsewhere.

---
+++ data/contacts.js
@@ -0,0 +1,13 @@
+exports.items = [{
+    'href': 'http://www.twitter.com/fulljames',
+    'label': 'Twitter'
+},{
+    'href': 'http://www.flickr.com/emptyjames',
+    'label': 'Flickr'
+},{
+    'href': 'http://instagrid.me/fulljames/',
+    'label': 'Instagrid'
+},{
+    'href': 'http://uk.linkedin.com/in/stephenfulljames',
+    'label': 'Linked In'
+}];
---
+++ data/tools.js
@@ -0,0 +1,31 @@
+exports.items = [{
+    'href': 'http://www.knockoutjs.com',
+    'label': 'Knockout',
+    'meta': 'Trending',
+    'description': 'Javascript library for UI data binding using the Model-View-View Model pattern. Essential for rapid prototyping.'
+},{
+    'href': 'http://www.lesscss.org',
+    'label': 'LESS',
+    'meta': 'Trending, used here',
+    'description': 'CSS pre-compiler adding rich dynamic behaviour, allowing more structured stylesheets and functional operations.'
+},{
+    'href': 'https://github.com/jquery/qunit',
+    'label': 'QUnit',
+    'meta': 'QUnit',
+    'description': 'A powerful Javascript unit-testing framework. Don\'t start making a serious web app without it (or something like it).'
+},{
+    'href': 'http://nodejs.org',
+    'label': 'Node.js',
+    'meta': 'Trending, used here',
+    'description': 'Event-driven JavaScript server based on the V8 engine. Still getting to grips with this one but liking what I see.'
+},{
+    'href': 'http://grabaperch.com',
+    'label': 'Perch',
+    'meta': 'Trending',
+    'description': 'A brilliant little CMS that gives simple integration of powerful features into any HTML template.'
+},{
+    'href': 'http://www.slimframework.com/',
+    'label': 'Slim Framework',
+    'meta': 'Trending',
+    'description': 'At heart a straightforward Restful router framework for PHP, with some powerful features for quick and easy development.'
+}];
