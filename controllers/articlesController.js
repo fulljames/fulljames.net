2f5149d5a8de569900ca6977a1cf43bbf6626ee5
Stephen Fulljames <stephen@fulljames.net>
2012-08-21 16:32:49 +0100

    Added page route for articles. Abstracted contact links into a partial. 404 page for broken article routes.

--- controllers/articlesController.js
+++ controllers/articlesController.js
@@ -1,3 +1,5 @@
+var _ = require('underscore');
+
 exports.index = function(req, res) {
 
     var contacts = require('../data/contacts.js');
@@ -16,8 +18,33 @@ exports.index = function(req, res) {
             'title': 'Elsewhere',
             'links': writing.items
         }
-    }
+    };
 
     res.render("articles/index", { title: 'stephen fulljames - articles', data: data });
 
+};
+
+exports.page = function(req,res, next) {
+    if (req.url.indexOf('favicon.ico') != -1) return false;
+
+    var contacts = require('../data/contacts.js');
+    var articles = require('../data/articles.js');
+
+    var selectedArticle = _.find(articles.items, function(item) {
+        return item.href == req.params.article;
+    });
+
+    var data = {
+        'contacts': {
+            'links': contacts.items
+        },
+        'article': selectedArticle
+    };
+
+    if (!selectedArticle) {
+        res.render("errors/404", { title: 'stephen fulljames - 404', data: data} );
+        next();
+    }
+
+    res.render("articles/page", { title: 'stephen fulljames - articles', data: data })
 };
--- data/articles.js
+++ data/articles.js
@@ -3,9 +3,4 @@ exports.items = [{
     'label': 'On Cross-Origin-Resource-Sharing',
     'meta': '21 Aug 2012',
     'abstract': 'Nulla facilisi. Duis aliquet egestas purus in blandit. Curabitur vulputate, ligula lacinia scelerisque tempor, lacus lacus ornare ante, ac egestas est.'
-},{
-    'href': 'on-cors',
-    'label': 'On Cross-Origin-Resource-Sharing',
-    'meta': '21 Aug 2012',
-    'abstract': 'Nulla facilisi. Duis aliquet egestas purus in blandit. Curabitur vulputate, ligula lacinia scelerisque tempor, lacus lacus ornare ante, ac egestas est.'
 }];
