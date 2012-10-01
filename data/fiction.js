7c3cce763dcd1018fb0253d32b54156403946157
Stephen Fulljames <stephen@fulljames.net>
2012-10-01 11:22:45 +0100

    Fiction 02 added.

--- data/fiction.js
+++ data/fiction.js
@@ -1,4 +1,16 @@
 exports.items = [{
+    'href': 'lag',
+    'index': '02',
+    'label': 'Lag',
+    'meta': '28 Sep 2012',
+    'live': true,
+    'abstract': "Thanh crouched in a gutter of black, slimy water feeling shit and disease seeping through every stitch of his tattered combats.",
+    'context': 'Lag originates from around 2006, from a monthly writing competition on a video games forum. The title was the theme word for that month.',
+    // custom css for this article
+    'css': null,
+    // custom js for this article
+    'js': null
+},{
     'href': 'double',
     'index': '01',
     'label': 'Double',
