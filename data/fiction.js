0a9ec41dadbb558e7abca1abec0b6d4f8791b58d
Stephen Fulljames <stephen@fulljames.net>
2012-10-05 14:18:00 +0100

    Fiction Friday 03.

--- data/fiction.js
+++ data/fiction.js
@@ -1,4 +1,16 @@
 exports.items = [{
+    'href': 'the-kid-who-believes-hes-galactus',
+    'index': '03',
+    'label': "The Kid Who Believes He's Galactus",
+    'meta': '05 Oct 2012',
+    'live': true,
+    'abstract': "One of the kids here, have I told you about him? He must be about nine, I suppose, skinny, and thinks he's Galactus.",
+    'context': 'A chapter from a writing game called <a href="http://www.mappalujo.com/">Mappalujo</a>, devised by Jeff Noon and Steve Beard, which I played with <a href="http://oletheros.com">R. M. Rhodes</a> a few years ago.',
+    // custom css for this article
+    'css': null,
+    // custom js for this article
+    'js': null
+},{
     'href': 'lag',
     'index': '02',
     'label': 'Lag',
